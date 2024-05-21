63fb4c3f1a81ef26b88ecc0f2f7edd7eec2d4a82
SalihEris <335385@student.mboutrecht.nl>
2024-05-21 14:45:29 +0200

    edited model

--- app/models/Appointments.php
+++ app/models/Appointments.php
@@ -4,15 +4,7 @@ class Appointments
 {
     private $db;
 
-    public function __construct()
-    {
-        /**
-         * Maak een nieuw database object die verbinding maakt met de 
-         * MySQL server
-         */
-        $this->db = new Database();
-    }
-
+    
     public function getAppointment()
     {
         try {
